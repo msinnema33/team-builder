d09fe79cf1ac8d2a1ddf79b0bc1ac331a2e9ddee
Mike Sinnema <msinnema33@gmail.com>
2020-01-29 16:08:03 -0700

    problems with map in Members

--- src/App.js
+++ src/App.js
@@ -1,40 +1,48 @@
 import React, { useState } from 'react';
 import { Route, Switch, useHistory } from 'react-router-dom';
-
+import Members from './Members';
 import './App.css';
-import Form from './Form';
+import Form from './Form.js';
 
 function App() {
-  const [member, useMember] = useState({firstName: "", lastName: "", role: "", email: "", id: "" });
-  const team = [];
+  const [members, setMembers] =useState({
+    firstName: 'Mike',
+    lastName: 'Sinnema',
+    role: 'Project Manager',
+    email: 'mjs@mail.com',
+    id: 1
+    });  
+  
   const history = useHistory();
   const routeToForm = event => {
     history.push("/Form");
   }
+  const addNewTeam = member => {
+    const newTeam = {
+      firstName: member.firstName,
+      lastName: member.lastName,
+      role: member.role,
+      email: member.email,
+      ID: Date.now(),
+    };
+    setMembers([...members, newTeam]);
+  };
 
   return (
     <div className="App">
       <Switch>
         <Route exact path='/Form'>
-           <Form />
+           <Form addNewTeam = {addNewTeam}/>
         </Route>
       </Switch>
       <header className="App-header">
-      {/* <Link to = '/Form'>   */}
-      <button onClick ={routeToForm} className="md-button shop-button">Add a Team member</button>
-      {/* </Link> */}
-      
+        <button onClick ={routeToForm} className="md-button shop-button">Add a Team member</button>
+            
         Welcome to my Team!<br> 
       </br>
         Role      Last        First       Email      
-
+       <Members members = {members} />
       </header> 
-      {/* <Switch>
-        <Route exact path='/Form'>
-           <Form />
-        </Route>
-      </Switch> */}
-      
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -1,54 +1,57 @@
 import React, { useState } from 'react';
 
-function Form = props => {
-const [formData, setFormData] =useState({
-  firstName: '',
-  lastName: '',
-  role: '',
-  email: '',
-  ID: '',
-})
+const Form = props => {
+const [member, setMember] = useState([{firstName: "", lastName: "", role: "", email: ""}]);  
 
 const onInputChange = event => {
-  setFormData({
-    ...formData,
+  setMember({
+    ...member,
     [event.target.name]: event.target.value,
   });
 }
 
 const submitForm = event => {
-  event.preventDefault
-  addNewTeam
+  event.preventDefault();
+  props.addNewTeam(member);
+  setMember({ 
+  firstName: '',
+  lastName: '',
+  role: '',
+  email: '',
+ });
 }
-
     return (
       <div className="Form">
-        <form>
+        <form onSubmit={submitForm}>
           <label>
             First Name:
             <input name = 'firstName' onChange ={onInputChange} type = 'text'
-            placeholder = 'First Name'/>
+            placeholder = 'First Name'
+            value = {member.firstName}/>
           </label><br></br>
           <label>
             Last Name:
             <input name = 'lastName' onChange ={onInputChange} type = 'text'
-            placeholder = 'Last Name' />
+            placeholder = 'Last Name' 
+            value = {member.lastName}/>
           </label><br></br>
           <label>
             Role:
             <input name = 'role' onChange ={onInputChange} type = 'text'
-            placeholder = 'role'/>
+            placeholder = 'role'
+            value = {member.role}/>
           </label><br></br>
           <label>
             Email address:
             <input name = 'email' onChange ={onInputChange} type = 'email'
-            placeholder = 'valid email'/>
+            placeholder = 'valid email'
+            value = {member.email}/>
           </label><br></br>
-          <label>
+          {/* <label>
             ID Number:
             <input name = 'ID' onChange ={onInputChange} type = 'number'
             placeholder = 'Badge number'/>
-          </label>
+          </label> */}
         </form>
       </div>
     );
